7bd0b20c608648975b80e16ea1065ca95689cc5b
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-01 08:42:24 +0300

    ...

--- Note.Core/Models/Note.cs
+++ Note.Core/Models/Note.cs
@@ -7,6 +7,8 @@ namespace Note.Core.Models
 {
     public class Note
     {
+       public const int MAX_LENGTH_OF_TITLE = 50;
+        public const int MAX_LENGTH_OF_DESCRIPTION = 250;
 
         private Note(Guid _id, string _Title, string _Desrip)
         {
@@ -24,11 +26,23 @@ namespace Note.Core.Models
         public bool isComplete { get; set; } = false;
 
 
-        public static (Note note, string Error) Create (Guid _id, string _Title, string _Desrip)
+        public static (Note note, string Error) Create (Guid _id, string _Title, string _Descrip)
         {
             var error = string.Empty;
             
-            var note = new Note (_id, _Title, _Desrip);
+            if(string.IsNullOrEmpty(_Descrip) || _Descrip.Length > MAX_LENGTH_OF_DESCRIPTION )
+            {
+                error = "Description is can not be Empty or longer then 250 symbols";
+
+            }
+
+            if (string.IsNullOrEmpty(_Title) || _Title.Length > MAX_LENGTH_OF_TITLE)
+            {
+                error = "Title is can not be Empty or longer then 50 symbols";
+
+            }
+
+            var note = new Note (_id, _Title, _Descrip);
 
             return (note, error);
         }
