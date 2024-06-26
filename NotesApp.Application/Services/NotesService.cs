6f4582393dc0499757fa4f87c080c1a27b0a3ee0
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-26 18:26:23 +0300

    Oke, connect sqlserver Dependencies, and Methods for Servecies LE go

---
+++ NotesApp.Application/Services/NotesService.cs
@@ -0,0 +1,27 @@
+﻿
+using NotesApp.Core.Models;
+using NotesApp.Repositories;
+
+namespace NotesApp.Application.Services
+{
+    public class NotesService
+    {
+        private readonly INotesRepository _notesRepository;
+
+        public NotesService(INotesRepository notesRepository)
+        {
+            _notesRepository = notesRepository;
+        }
+
+
+        public async Task<List <Note>> GetAllNotes()
+        {
+            return await _notesRepository.Get();
+        }
+
+        public async Task<Guid> CreateNote(Note note)
+        {
+            return await _notesRepository.Create(note);
+        }
+    }
+}
