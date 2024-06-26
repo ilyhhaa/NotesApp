326de609c9c0e0caa690d9558b51138d672f8e99
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-26 17:37:43 +0300

    Don't ask

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NotesApp.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Note_DB_Context>(options=>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(Note_DB_Context)));
+});
+
 
 var app = builder.Build();
 
