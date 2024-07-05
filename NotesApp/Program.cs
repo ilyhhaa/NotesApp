fa30fb23439da3f29f3ee0982e311a0a99ef381f
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-05 16:37:33 +0300

    UseCors()

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -33,4 +33,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x =>
+{
+    x.WithHeaders().AllowAnyHeader();
+    x.WithOrigins("http:localhost:3000");
+    x.WithMethods().AllowAnyMethod();
+});
+
 app.Run();
